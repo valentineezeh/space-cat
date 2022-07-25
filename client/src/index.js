b404ea18aebb3324f8cf0ac90c39d68d8a0bfe08
valentineEze <v.ezeh@newmotion.com>
2022-07-25 14:17:49 +0200

    add docker file to frontend

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import Pages from './pages';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client'
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  uri: process.env.REACT_APP_BACKEND_API_URL,
   cache: new InMemoryCache()
 })
 
